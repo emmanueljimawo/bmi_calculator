fe2923533b043a4d27189fe93b66c693381438f8
emmanuel <emmanueljimawo@yahoo.co.uk>
2020-04-11 00:18:20 +0100

    initial commit

--- bmi-calculator-flutter-master/lib/main.dart
+++ bmi-calculator-flutter-master/lib/main.dart
@@ -6,7 +6,11 @@ class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      theme: ThemeData.dark(),
+      theme: ThemeData(
+        accentColor: Color(0xFF0A0E21),
+        primaryColor: Color(0xFF0A0E21),
+        scaffoldBackgroundColor: Color(),
+      ),
       home: InputPage(),
     );
   }
